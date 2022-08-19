732e7ef44063b249f59f4b7a64b89eddade85219
ico organic <icoorganic@gmail.com>
2022-08-19 14:04:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const conectarDB = require('./config/db');
 const cors = require("cors");
+require('dotenv').config()
 
 // Creamos el servidor
 const app = express();
@@ -8,16 +9,14 @@ const app = express();
 // Conectamos a la BD 
 /* 
 pass: 0NTKzI9rN2hWVl3M 
-
 mongodb+srv://icoorganic:0NTKzI9rN2hWVl3M@cluster0.czlttyz.mongodb.net/test 
-
 mongodb+srv://icoorganic:0NTKzI9rN2hWVl3M@cluster0.czlttyz.mongodb.net/?retryWrites=true&w=majority 
-
 */
 conectarDB();
 app.use(cors())
 
 app.use(express.json());
+const PORT = process.env.PORT
 
 /**
  * mongodb+srv://icoorganic:0NTKzI9rN2hWVl3M@cluster0.czlttyz.mongodb.net/?retryWrites=true&w=majority 
@@ -27,6 +26,6 @@ app.use('/parcela', require('./routes/parcela'));
 app.use('/usuarios', require('./routes/usuarios'));
 app.use('/servicios', require('./routes/servicios'));
 
-app.listen(4200, () => {
+app.listen(PORT, () => {
     console.log('El servidor esta corriendo perfectamente')
 })
